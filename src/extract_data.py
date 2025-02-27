08f2786796c8c70d66797d1001b0232361fe91cc
nimish <nimishpurohitn@gmail.com>
2025-02-27 23:16:53 +0530

    Minor fixes

--- src/extract_data.py
+++ src/extract_data.py
@@ -6,83 +6,81 @@ import time
 # Function to get the response object from Yahoo.com's finance page
 # Top news articles of the moment, in the response object, are  tags of class 'js-content-viewer'
 
+
 def get_page(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
     response = requests.get(url, headers=headers)
     if not response.ok:
-        print('Status code:', response.status_code)
-        raise Exception(f'Failed to load page {url}')
+        print("Status code:", response.status_code)
+        raise Exception(f"Failed to load page {url}")
     page_content = response.text
-    doc = BeautifulSoup(page_content, 'html.parser')
+    doc = BeautifulSoup(page_content, "html.parser")
     return doc
 
 
-my_url = "https://finance.yahoo.com/news/"
+def return_data():
+    my_url = "https://finance.yahoo.com/news/"
 
-doc = get_page(my_url)
+    doc = get_page(my_url)
 
-a_tags = doc.find_all('div', {'class': "content yf-82qtw3"})
-articles = []
+    a_tags = doc.find_all("div", {"class": "content yf-82qtw3"})
+    articles = []
 
-for i in range(min(15,len(a_tags))):      #restricting to 25 articles
-  articles.append(a_tags[i].find('a'))
+    for i in range(min(15, len(a_tags))):  # restricting to 15 articles
+        articles.append(a_tags[i].find("a"))
 
-  
-# Searching for and saving the links to articles embedded in the  tags
+    # Searching for and saving the links to articles embedded in the  tags
 
-links = []
-url = ""                 #https://finance.yahoo.com/news
+    links = []
+    url = ""  # https://finance.yahoo.com/news
 
-for anchor in articles:
+    for anchor in articles:
 
-  href=anchor.get('href')
-  links.append(url+href)
+        href = anchor.get("href")
+        links.append(url + href)
 
-print(links)
+    # print(links)
 
+    # Extracting the text from the actual news article using the links stored before
 
-# Extracting the text from the actual news article using the links stored before
+    articles_text = []
+    headlines = []
 
-articles_text = []
-headlines = []
+    for link in links:
 
+        if (
+            link.find("video") == -1
+        ):  # Excluding news links with videos instead of articles
 
-for link in links:
+            curr = ""
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            }
+            res = requests.get(link, headers=headers)
 
-  if link.find('video')==-1:                      # Excluding news links with videos instead of articles
+            sleep_time = random.uniform(1, 4)  # Wait between 2 to 5 seconds
+            time.sleep(sleep_time)
 
-    curr = ""
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-    }
-    res = requests.get(link, headers=headers)
+            article = BeautifulSoup(res.text, "html.parser")
 
-    sleep_time = random.uniform(1, 4)  # Wait between 2 to 5 seconds
-    time.sleep(sleep_time)
+            # print(article)
+            outer = article.find_all("div", {"class": "body yf-tsvcyu"})
+            title = article.find("div", {"class": "cover-title yf-1rjrr1"})
 
+            if outer != []:
+                paras = outer[0].find_all("p")
 
-    article = BeautifulSoup(res.text, 'html.parser')
+                for i in range(
+                    1, len(paras) - 1
+                ):  # Excluding  tags with irrelevant info. like date, time, author etc.
+                    curr += paras[i].text
+                articles_text.append(curr)
 
-    #print(article)
-    outer = article.find_all('div',{'class':'body yf-tsvcyu'})
-    title = article.find('div',{'class':'cover-title yf-1rjrr1'})
+                headlines.append(title.text)
 
+    print("Number of articles: ", len(articles_text))
+    # print(headlines)
 
-    if outer!= []:
-      paras = outer[0].find_all('p')
-
-      for i in range(1,len(paras)-1):               # Excluding  tags with irrelevant info. like date, time, author etc.
-        curr+=paras[i].text
-      articles_text.append(curr)
-
-
-      headlines.append(title.text)
-
-
-
-print('Number of articles: ',len(articles_text))
-print(headlines)
-
-
+    return articles_text
--- src/query_pipeline.py
+++ src/query_pipeline.py
@@ -1,20 +1,25 @@
 import argparse
 from inference_api import query_response
 from transformers import pipeline
+from extract_data import return_data
 
 
 class Pipeline:
     def __init__(self, query="", path=""):
         self.query, self.path = query, path
+        self.context = return_data()
+        print(self.context)
+        # self.summary = self.summarize_articles(self.context)
 
     def generate_response(self):
         return query_response(self.query)
-    
 
-    def summarize_articles(articles_text, max_length=300, min_length=100):
+    def summarize_articles(self, articles_text, max_length=300, min_length=100):
         summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-        summary = summarizer(articles_text, max_length=max_length, min_length=min_length, do_sample=False)
-        return summary[0]['summary_text']
+        summary = summarizer(
+            articles_text, max_length=max_length, min_length=min_length, do_sample=False
+        )
+        return summary[0]["summary_text"]
 
 
 def parse_arguments():
